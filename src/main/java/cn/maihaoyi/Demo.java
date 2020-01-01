10412d0876b185b1863bf9489fe3e2f2b7408fe0
maihaoyi <110317449@qq.com>
2020-01-01 11:09:09 +0800

    回到版本1

--- src/main/java/cn/maihaoyi/Demo.java
+++ src/main/java/cn/maihaoyi/Demo.java
@@ -3,7 +3,5 @@ package cn.maihaoyi;
 public class Demo {
     public static void main(String[] args) {
         System.out.println(11);
-        System.out.println(22);
-        System.out.println(33);
     }
 }
