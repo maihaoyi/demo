105818546917782415407ecc14a7ea0b335a3c62
maihaoyi <110317449@qq.com>
2020-01-01 11:11:43 +0800

    回到最新版本

--- src/main/java/cn/maihaoyi/Demo.java
+++ src/main/java/cn/maihaoyi/Demo.java
@@ -3,5 +3,7 @@ package cn.maihaoyi;
 public class Demo {
     public static void main(String[] args) {
         System.out.println(11);
+        System.out.println(22);
+        System.out.println(33);
     }
 }
